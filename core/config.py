9ff3d6d5c79047d2ce45859ab84cc2646dc89645
Utkarsh Raj <utkarsh29raj10@gmail.com>
2026-06-28 08:03:29 +0530

    Implemented Wakatime Confiuration Manager to write to .wakatime file with default privacy settings

---
+++ core/config.py
@@ -0,0 +1,53 @@
+import os
+import configparser
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+class ConfigManager:
+    DEFAULT_API_URL=""
+
+    def __init__(self):
+        self.home_dir = os.path.expanduser("~")
+        self.config_path = os.path.join(self.home_dir, ".wakatime.cfg")
+
+    def write_config(self, api_key: str, api_url: str = "") -> bool:
+        if not api_key:
+            logging.error("Failed to write config: Please enter API key")
+            raise ValueError("API key cannot be empty")
+
+        final_url = api_url.strip() if api_url else self.DEFAULT_API_URL
+
+        config = configparser.ConfigParser()
+
+        if os.path.exists(self.config_path):
+            config.read(self.config_path)
+
+        if not config.has_section('settings'):
+            config.add_section('settings')
+
+        config.set('settings', 'api_key', api_key)
+        config.set('settings', 'api_url', final_url)
+        config.set('settings', 'heartbeat_rate_limit_seconds', '30')
+        config.set('settings', 'exclude_unknown_project', 'true')
+        config.set('settings', 'hide_branch_names', 'true')
+
+        try:
+            with open(self.config_path, 'w') as configfile:
+                config.write(configfile)
+            logging.info(f"Successfully wrote WakaTime configuration to {self.config_path}")
+            return True
+
+        except IOError as e:
+            logging.error(f"Failed to write to {self.config_path}: {e}")
+            raise
+
+# Manual Testing
+if __name__ == "__main__":
+    print("Testing ConfigManager")
+    try:
+        manager = ConfigManager()
+        manager.write_config(api_key="test-api-blah-blah", api_url="")
+        print ("Successfully checked your home directory for ~/.wakatime.cfg")
+    except Exception as error:
+        print(f"Testing failed: {error}")
